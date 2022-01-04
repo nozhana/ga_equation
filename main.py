65eeec9cec06238e2ce02b7ae10f10bd5fb9852c
Nozhan Amiri <nozhana@gmail.com>
2022-01-05 01:36:59 +0330

    Initial commit

---
+++ main.py
@@ -0,0 +1,2 @@
+'''Main code to generate solution 'chromosome' generations using the
+    ga module'''
---
+++ modules/ga.py
@@ -0,0 +1,2 @@
+'''Genetic Algorithm implemented to solve the following equation
+    y = w1x1 + w2x2 + w3x3 + w4x4 + w5x5 + w6x6'''
