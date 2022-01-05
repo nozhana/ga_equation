168cd1fb0ff2ccdfa30571afc51c17d8a89afd2b
Nozhan Amiri <nozhana@gmail.com>
2022-01-05 04:21:37 +0330

    First algorithm

--- main.py
+++ main.py
@@ -1,2 +1,63 @@
-'''Main code to generate solution 'chromosome' generations using the
-    ga module'''
+'''Main code to generate solution generations to the following
+    equation using the ga module
+    y = w1x1 + w2x2 + w3x3 + w4x4 + w5x5 + w6x6'''
+
+from modules import ga
+import numpy as np
+
+# Values of x1 through x6
+xvalues = np.array([4, -2, 3.5, 5, -11, -4.7])
+
+# Number of weights to be optimized
+nweights = 6
+
+# Number of solutions per population
+nsols = 8
+
+# Solution range
+low, high = -4.0, 4.0
+
+# Number of generations to run
+ngens = 50
+
+# Number of mating parents to extract
+nparents = 4
+
+# Number of offsprings to be generated
+noffsprings = nsols - nparents
+
+new_pop = ga.new_pop(low, high, (nsols, nweights))
+print('Initial population\n')
+print(new_pop)
+print('\n\n')
+
+for gen in range(1, ngens+1):
+    print('Generation ' + str(gen) + '\n\n')
+    fitness = ga.pop_fitness(xvalues, new_pop)
+    print('Fitness values gen ' + str(gen) + '\n')
+    print(fitness)
+    print('\n\n')
+    parents = ga.sel_parents(new_pop, fitness, nparents)
+    print('Parents gen ' + str(gen) + '\n')
+    print(parents)
+    print('\n\n')
+    crossed = ga.crossover(parents, (noffsprings, nweights))
+    print('Crossovers gen ' + str(gen) + '\n')
+    print(crossed)
+    print('\n\n')
+    mutated = ga.mutate(crossed, (low, high))
+    print('Mutates gen ' + str(gen) + '\n')
+    print(mutated)
+    print('\n\n')
+    new_pop[:nparents] = parents
+    new_pop[nparents:] = mutated
+    print('Next gen population (gen ' + str(gen+1) + ')\n')
+    print(new_pop)
+    print('\n\n')
+    print('Best fitness after gen ' + str(gen) + '\n')
+    print(np.max(ga.pop_fitness(xvalues, new_pop)))
+    print('\n\n')
+
+print('BEST SOLUTION AFTER {} GENS:\n'.format(ngens))
+print(new_pop[ga.pop_fitness(xvalues, new_pop).argsort()[-1]])
+print('\n\n')
--- modules/ga.py
+++ modules/ga.py
@@ -1,2 +1,44 @@
 '''Genetic Algorithm implemented to solve the following equation
+    for given x values
     y = w1x1 + w2x2 + w3x3 + w4x4 + w5x5 + w6x6'''
+
+import numpy as np
+
+# Mutation index
+# mutidx = 4
+
+# Creating the initial population
+def new_pop(low, high, pop_size):
+    return np.random.uniform(low=-4.0, high=4.0, size=pop_size)
+
+# Calculate the fitness for individual solutions in population
+def pop_fitness(xs, pop):
+    return np.sum(pop * xs, axis=1)
+
+# Calculate the fitness for an individual solution
+# def sol_fitness(xs, sol):
+#     pass
+
+# Select the mating pool -- a.k.a 'parents' to mate
+def sel_parents(pop, fitness, nparents):
+    return pop[fitness.argsort()][-nparents:]
+
+# Generate offspring using crossover breeding
+def crossover(parents, offspring_size):
+    offspring = np.empty(offspring_size)
+    crossover_point = np.uint8(offspring_size[1]/2)
+
+    for k in range(offspring_size[0]):
+        parent1idx = k % parents.shape[0]
+        parent2idx = (k + 1) % parents.shape[0]
+        offspring[k, :crossover_point] = parents[parent1idx, :crossover_point]
+        offspring[k, crossover_point:] = parents[parent2idx, crossover_point:]
+
+    return offspring
+
+# Mutate crossover offsprings
+def mutate(offspring, range):
+    mutidx = np.random.randint(offspring.shape[1])
+    offspring[:, mutidx] += np.random.uniform(-1.,1.,offspring.shape[0])
+    offspring[:, mutidx] = np.clip(offspring[:, mutidx], range[0], range[1])
+    return offspring
